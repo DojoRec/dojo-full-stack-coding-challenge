2212b835a30201a617aa5c45f47c0e04a8c8731d
Leon Kemp <leon.kemp@paymentsense.com>
2022-03-08 11:34:30 +0000

    Projects added

---
+++ Dojo Full Stack Coding Challenge/paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Models/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Paymentsense.Coding.Challenge.Api.Models
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public string Population { get; set; }
+        public string Alpha3Code { get; set; }
+        public string Region { get; set; }
+        public string Subregion { get; set; }
+        public IEnumerable<Language> Language { get; set; }
+        public IEnumerable<Currency> Currency { get; set; }
+    }
+}
---
+++ Dojo Full Stack Coding Challenge/paymentsense-coding-challenge-website/src/app/models/country.model.ts
@@ -0,0 +1,13 @@
+import { Currency } from "./currency.model";
+import { Language } from "./language.model";
+
+export interface Country {
+    Name: string;
+    Population: number;
+    Alpha3Code: string;
+    region: string;
+    subregion: string;
+    languages: Language[]; 
+    currencies: Currency[];
+}
+
